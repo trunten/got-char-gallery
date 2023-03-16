3bff3bf937b6edf036c243c811b57daa853a3a37
Jonathan Davis <trunten@btinternet.com>
2023-03-16 21:05:32 +0000

    stylin'

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,19 +1,27 @@
 export default function Character({height, race, gender, birth, spouse, death, realm, hair, name, wikiUrl, imgUrl}) {
   return (
-    <div data-test="component-character">
-      <h2 data-test="char-name">{name}</h2>
-      <img data-test="char-img" src={imgUrl} alt={name}></img>
-      <ul data-test="char-list">
-        {gender && <li data-test="char-gender">Gender: {gender}</li>}
-        {race && <li data-test="char-race">Race: {race}</li>}
-        {realm && <li data-test="char-realm">Realm: {realm}</li>}
-        {spouse && <li data-test="char-spouse">Spouse: {spouse}</li>}
-        {birth && <li data-test="char-birth">Date of birth: {birth}</li>}
-        {death && <li data-test="char-death">Date of Death: {death}</li>}
-        {height && <li data-test="char-height">Height: {height}</li>}
-        {hair && <li data-test="char-hair">Hair: {hair}</li>}
-        {wikiUrl && <li data-test="char-hair"><a href={wikiUrl}>Wiki</a></li>}
-      </ul>
+    <div className="col-sm-6 col-lg-4 mb-4">
+      <div data-test="component-character" className="card h-100 shadow">
+        <div className="card-header">
+          <h2 data-test="char-name" className="mb-4 text-center">{name}</h2>
+          {/* <div className="img-wrapper card-img-top p-4" style={{height:"300px"}}> */}
+            <img data-test="char-img" src={imgUrl} alt={name} className="card-img-top d-block mx-auto mb-4" style={{height: "auto", width: "auto", maxHeight: "200px", minHeight: "200px"}}></img>
+          {/* </div> */}
+        </div>
+        <div className="card-body d-flex flex-column justify-content-between">
+          <ul data-test="char-list" className="list-unstyled">
+            {gender && <li data-test="char-gender"><b>Gender:</b> {gender}</li>}
+            {race && <li data-test="char-race"><b>Race:</b> {race}</li>}
+            {realm && <li data-test="char-realm"><b>Realm:</b> {realm}</li>}
+            {spouse && <li data-test="char-spouse"><b>Spouse:</b> {spouse}</li>}
+            {birth && <li data-test="char-birth"><b>Date of birth:</b> {birth}</li>}
+            {death && <li data-test="char-death"><b>Date of Death:</b> {death}</li>}
+            {height && <li data-test="char-height"><b>Height:</b> {height}</li>}
+            {hair && <li data-test="char-hair"><b>Hair:</b> {hair}</li>}
+          </ul>
+          {wikiUrl && <a data-test="char-wiki" className="mt-2 btn btn-outline-dark w-100 stretched-link" href={wikiUrl}>Wiki</a>}
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/CharacterGallery.js
+++ src/components/CharacterGallery.js
@@ -2,7 +2,7 @@ import Character from "./Character";
 import json from "../data/characterData.json";
 export default function CharacterGallery() {
   return (
-    <div data-test="component-char-gallery" className="container-fluid">
+    <div data-test="component-char-gallery" className="container-fluid my-5 mx-auto" style={{maxWidth:"1400px"}}>
       <div className="row">
         {json.map(character => <Character key={character._id} {...character} />)}
       </div>
